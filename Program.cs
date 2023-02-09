3d5f5fd6e0aff3e242328351fb03609e0ae41f13
tanya <zharik08@mail.ru>
2023-02-10 00:16:40 +0800

    Написание кода решения задачи с помощью Void-метода

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк
+// формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте
+// выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+string [] array = {"Russia", "Denmark", "Kazan", "Ufa"};
+string [] newArray = new string [array.Length];
+
+void PrintArray (string [] array)       // метод, который выводит массив строк на экран
+{
+   //int count = array.Length;
+   for (int i = 0; i < array.Length; i++)
+   {
+    Console.Write($"{array[i]} ");
+   }
+   Console.WriteLine();
+   Console.WriteLine("-->");
+}
+
+void ResArray (string [] array, string [] newArray)   // метод, который находит элементы строк, длина которых меньше или равна 3 симв.
+{
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[index] = array[i];
+            index++;
+        }
+    }
+}
+
+void PrintNewArray (string [] newArray)       // метод, который выводит новый массив строк на экран
+{
+   //int count = array.Length;
+   for (int i = 0; i < newArray.Length; i++)
+   {
+    Console.Write($"{newArray[i]} ");
+   }
+   Console.WriteLine();
+}
+
+PrintArray(array);
+ResArray(array, newArray);
+PrintNewArray(newArray);
+
+
+
+
+
+
